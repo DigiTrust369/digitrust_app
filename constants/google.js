f6980990e47b2c6f73986467ec2e01fc2ca0d4c5
lamdanghoang <danghlambk14@gmail.com>
2024-05-23 16:47:41 +0700

    test add api submit gg sheet

---
+++ constants/google.js
@@ -0,0 +1,7 @@
+const google = {
+  CLIENT_EMAIL: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_EMAIL,
+  PRIVATE_KEY: process.env.NEXT_PUBLIC_GOOGLE_PRIVATE_KEY,
+  SHEET_ID: process.env.NEXT_PUBLIC_GOOGLE_SHEET_ID,
+};
+
+export default google;
